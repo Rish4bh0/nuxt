63a329670e1b929ffca768643fe7f9975b95e17d
Rish4bh0 <122444548+Rish4bh0@users.noreply.github.com>
2024-04-01 14:06:43 +0545

    Initialized nuxt project build navbar and sidebar

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  pages: true,
+  modules: [
+    'nuxt-icon', 
+    '@pinia/nuxt', 
+    '@pinia-plugin-persistedstate/nuxt'
+  ],
+  build: {
+    transpile: ['pinia-plugin-persistedstate'],
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
+});
